c902b4c3e4f0313dca7d501d1c388b914d7d7570
Ryan-Donovan33 <55716909+Ryan-Donovan33@users.noreply.github.com>
2020-02-20 20:09:39 -0500

    Added ContactForm test file

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, getByText } from '@testing-library/react';
 import App from './App';
 import 'mutationobserver-shim';
 
 test('app renders without crashing!', () => {
 	render(<App />);
+});
+
+test('basic test to see if working', () => {
+	const { getByText } = render(<App />);
+
+	const header = getByText(/Testing/i);
+
+	expect(header).toBeInTheDocument();
 });
